39c080b3ef29e06708688492db13654d0767a18f
Moritz Köhler <MK@Moritzs-MacBook-Pro.local>
2019-11-12 16:32:59 +0100

    191112

---
+++ mk/config.py
@@ -0,0 +1,10 @@
+
+# scrape new keys needed for the API from the website directly as soon as
+# the old expire
+
+
+cfg = {'consumer_key': 'EHgpubVOu8GuK9rt4aqrd8HU2',
+        'consumer_secret': 'HyNzR0er6xiD3o5c9F1QOjKyyHTc9Qo5Jhy1C3PxNtQLKciwYE',
+        'access_token' : '1193838230661193728-8WbbDJblNAYkof0m3VODqnn8AtOCME',
+        'access_token_secret': 'sbvMijOl4uYPAkIerl1XbvIYNadPjpCfegMQ4G87nGVxd'
+        }
