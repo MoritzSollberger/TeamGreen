9bf66af3bb82dba4aadc716b046dbfa50a333716
MoritzSollberger <moritz_so@hotmail.com>
2019-11-13 18:50:00 +0100

    updates

--- MoritzS/etl/dagtest.py
+++ MoritzS/etl/dagtest.py
@@ -1,17 +1,27 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
+from airflow.contrib.hooks.mongo_hook import MongoHook
+! pip install pymongo
+
+
+# from pymongo import MongoClient
+# CLIENT = MongoClient('localhost', 27017)
+# db = CLIENT.twitter_data
+
+# hook = MongoHook(conn_type = mongo)
+
 
 dag = DAG(
-	dag_id = 'my_first_dag',
-	start_date = datetime.now(),
+	dag_id = 'test_dag',
+	start_date = datetime(2019, 11, 13),
 	schedule_interval = '* * * * *')
+
+# def pull_latest_tweets():
+# 	for tweet in db.tweets.find()
 
 def print_hello():
 	return "hello!"
-
-def print_goodbye():
-	return "goodbye!"
 
 print_hello = PythonOperator(
 	task_id = 'print_hello',
@@ -20,10 +30,5 @@ print_hello = PythonOperator(
 	#dag param points to the DAG that this task is a part of
 	dag = dag)
 
-print_goodbye = PythonOperator(
-	task_id = 'print_goodbye',
-	python_callable = print_goodbye,
-	dag = dag)
-
 #Assign the order of the tasks in our DAG
-print_hello >> print_goodbye
+print_hello # >> next_task
